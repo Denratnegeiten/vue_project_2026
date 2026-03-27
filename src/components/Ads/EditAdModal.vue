f21f56efb5f7e25d6eab573135d414ba4669fc56
Den ratne geiten <panterra@panterra-linux.netis.cc>
2026-03-27 15:18:41 +0300

    17.1comm

---
+++ src/components/Ads/EditAdModal.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-dialog width="400px" v-model="modal">
+    <template v-slot:activator="{ props }">
+      <v-btn color="warning" v-bind="props" class="mr-3">Edit</v-btn>
+    </template>
+    <v-card>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card-title>
+              <h1 class="text--secondary">Edit Ad</h1>
+            </v-card-title>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row>
+          <v-col cols="12">
+            <v-card-text>
+              <v-text-field
+                name="title"
+                label="Title"
+                type="text"
+                v-model="editedTitle"
+              ></v-text-field>
+              <v-textarea
+                name="description"
+                label="Description"
+                type="text"
+                v-model="editedDescription"
+              ></v-textarea>
+            </v-card-text>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row>
+          <v-col cols="12">
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn @click="onCancel">Cancel</v-btn>
+              <v-btn color="success" @click="onSave">Save</v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['ad'],
+  data () {
+    return {
+      modal: false,
+      editedTitle: this.ad.title,
+      editedDescription: this.ad.description
+    }
+  },
+  methods: {
+    onCancel () {
+      this.editedTitle = this.ad.title
+      this.editedDescription = this.ad.description
+      this.modal = false
+    },
+    onSave () {
+      if (this.editedTitle !== '' && this.editedDescription !== '') {
+        // Логику сохранения добавим в 17.4
+        this.modal = false
+      }
+    }
+  }
+}
+</script>
