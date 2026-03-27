bac1c7e2a33c71e58a7d4174c1d451435f7491bb
Den ratne geiten <panterra@panterra-linux.netis.cc>
2026-03-27 13:07:45 +0300

    8.1comm

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,7 +1,35 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <h1>AdList</h1>
+    <v-row>
+      <v-col cols="12" sm="8" offset-sm="2">
+        <h1 class="text--secondary mb-3 mt-3">My Ads</h1>
+
+        <v-card 
+          class="elevation-10 mb-3" 
+          v-for="i in 3" 
+          :key="i"
+        >
+          <v-row no-gutters>
+            <v-col cols="4">
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+                height="160px"
+              ></v-img>
+            </v-col>
+            <v-col cols="8">
+              <v-card-text>
+                <h2 class="text--primary">Ad Title</h2>
+                <p>Ad Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info" to="/ad/1">Open</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+
+      </v-col>
     </v-row>
   </v-container>
 </template>
