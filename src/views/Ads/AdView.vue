7156518e726ab04ebae0f15c512634211ba0cb27
Den ratne geiten <panterra@panterra-linux.netis.cc>
2026-03-27 13:36:31 +0300

    9.2comm

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -2,9 +2,9 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <v-card>
+        <v-card class="mt-5">
           <v-img
-            :src="ad.src"
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
             height="300px"
           ></v-img>
           <v-card-text>
@@ -27,11 +27,9 @@ export default {
   data () {
     return {
       ad: {
-        title: "First ad",
-        desc: "This is a detailed description of the first ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        promo: true,
-        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-        id: "1"
+        title: 'First ad',
+        desc: 'Hello I am description',
+        id: '123'
       }
     }
   }
