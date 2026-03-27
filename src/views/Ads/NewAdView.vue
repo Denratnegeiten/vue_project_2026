9f1666dba3be9f5294df10975236f51ec8792fef
Den ratne geiten <panterra@panterra-linux.netis.cc>
2026-03-27 15:14:07 +0300

    16.5comm

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -86,18 +86,15 @@ export default {
     }
   },
   methods: {
-    createAd(){
-      if (this.$refs.form.validate()){
+    createAd () {
+      if (this.$refs.form.validate()) {
         const ad = {
           title: this.title,
-          desc: this.description,
+          description: this.description,
           promo: this.promo,
-          src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          imageSrc: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
         }
-        // Отправляем данные в Store [cite: 2066]
         this.$store.dispatch("createAd", ad)
-        
-        // После создания переходим на главную страницу 
         this.$router.push("/")
       }
     }
