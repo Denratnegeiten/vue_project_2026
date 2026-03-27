6539a766875fde88a7b02a6e5cb3517c060e5e46
Den ratne geiten <panterra@panterra-linux.netis.cc>
2026-03-27 16:09:05 +0300

    19.2comm

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -2,30 +2,32 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12" sm="8" lg="6">
-        <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+        <h1 class="text-secondary mb-3 mt-3">Orders</h1>
         
-        <v-list subheader two-line flat>
+        <v-list lines="two" flat>
           <v-list-item
             v-for="order in orders"
             :key="order.id"
           >
             <template v-slot:prepend>
-              <v-list-item-action>
-                <v-checkbox
-                  :input-value="order.done"
-                  color="primary"
-                  @click="markDone(order)"
-                ></v-checkbox>
-              </v-list-item-action>
+              <v-checkbox
+                :model-value="order.done"
+                color="primary"
+                @click.stop="markDone(order)"
+              ></v-checkbox>
             </template>
 
             <v-list-item-title>{{ order.name }}</v-list-item-title>
             <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
 
             <template v-slot:append>
-              <v-list-item-action>
-                <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
-              </v-list-item-action>
+              <v-btn 
+                color="primary" 
+                variant="elevated"
+                :to="'/ad/' + order.adId"
+              >
+                Open
+              </v-btn>
             </template>
           </v-list-item>
         </v-list>
@@ -37,23 +39,14 @@
 
 <script>
 export default {
-  data () {
-    return {
-      orders: [
-        {
-          id: "123",
-          name: "Kostya",
-          phone: "+7(978)000-00-05",
-          adId: "1",
-          done: true
-        }
-      ]
+  computed: {
+    orders () {
+      return this.$store.getters.orders
     }
   },
   methods: {
-    markDone(order) {
-      order.done = !order.done;
-      console.log(order.done);
+    markDone (order) {
+      this.$store.dispatch('markOrderDone', order.id)
     }
   }
 }
