e8d92464a15685d2df97533c09965827ba78c7f8
Den ratne geiten <panterra@panterra-linux.netis.cc>
2026-03-27 14:14:39 +0300

    13.1comm

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
 import ads from './ads'
-import user from './user' // Импорт модуля пользователя
+import user from './user'
+import shared from './shared'
 
 const store = createStore({
   modules: {
     ads,
-    user // Регистрация модуля
+    user,
+    shared
   }
 })
 
